9bfc9e6ddd32da19735aa2066ddf67016c71ef75
Niclas Eich <niclas2102@gmail.com>
2021-04-14 14:50:15 +0200

    Restructured envsetup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# get basedirectory
+export BTVHLTToolsDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+# source DeepJet env
+source $BTVHLTToolsDirectory/DeepJet/env.sh
+
