47ca12ce681e12b0561364fb193a96d907abbe30
Niclas Eich <niclas2102@gmail.com>
2021-04-14 15:02:25 +0200

    Env var for training output

--- setup.sh
+++ setup.sh
@@ -3,3 +3,20 @@ export BTVHLTToolsDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/nu
 # source DeepJet env
 source $BTVHLTToolsDirectory/DeepJet/env.sh
 
+if test -f "$BTVHLTToolsDirectory/local_setup.sh";
+then
+    source local_setup.sh
+else
+    echo "No local_setup.sh found!\n Using defaults for env vars"
+fi
+
+
+if [ -z "$TrainingOutput" ]
+then
+      echo "\$TrainingOutput is empty"
+      export TrainingOutput="${BTVHLTToolsDirectory}/training_output"
+      echo "Creating training-output directory!"
+      mkdir $TrainingOutput
+      echo "\$TrainingOutput set to ${TrainingOutput}"
+fi
+
--- submit/commands_training.sh
+++ submit/commands_training.sh
@@ -8,4 +8,4 @@ echo "Pythonpath:"
 echo $PYTHONPATH
 echo "current path:"
 pwd
-python3 $BTVHLTToolsDirectory/DeepJet/Train/train_DeepCSV.py /afs/cern.ch/work/n/neich/public/offline_djdc_files/files_04/dataCollection.djcdc /afs/cern.ch/work/n/neich/public/training_results/new_repo_tests/out_02
+python3 $BTVHLTToolsDirectory/DeepJet/Train/train_DeepCSV.py /afs/cern.ch/work/n/neich/public/offline_djdc_files/files_04/dataCollection.djcdc $TrainingOutput/out_02
