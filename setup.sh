fd99b47f0f83cf8a4569f71e25234a798dca63c0
Niclas Eich <niclas2102@gmail.com>
2021-07-19 14:14:26 +0200

    Training iteration

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 # get basedirectory
 export BTVHLTToolsDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # source DeepJet env
-export TrainingVersion="test_01"
+export TrainingVersion="test_15"
 export BTVHLTTrainingFile=/eos/cms/store/group/phys_btag/HLTRetraining/PhaseII/Online/HLTTDR_February2021/HLT_TRKv00_default/jet_shaped_2
 export OfflineDirectory=/eos/cms/store/group/phys_btag/HLTRetraining/PhaseII/Offline/Max_deepntuplizer_11_2_pv3d_newTrackCollection/pu140/
 export OfflineTrainingFiles=${OfflineDirectory}/djdc_files/${TrainingVersion}/dataCollection.djdc
-export OnlineDirectory=/eos/user/n/neich/HLT_BTV/nTuples/TRKRun3/jet_shaped_2
-export OnlineTrainingFiles=${OnlineDirectory}/djdc_files/${TrainingVersion}/dataCollection.djcdc
+export OnlineDirectory=/afs/cern.ch/work/n/neich/public/online_ntuples/TRKRun3/jet_shaped_splitted_test
+#export OnlineTrainingFiles=${OnlineDirectory}/djdc_files/${TrainingVersion}/dataCollection.djcdc
+export OnlineTrainingFiles=${OnlineDirectory}/djdc_files/test_14/dataCollection.djcdc
+export OnlineEvaluationFiles=${OnlineDirectory}/evaluation_filelist.txt
 source $BTVHLTToolsDirectory/DeepJet/env.sh
 export PYTHONPATH=$BTVHLTToolsDirectory:$PYTHONPATH
 export PYTHONPATH=$BTVHLTToolsDirectory/DeepJetCore:$PYTHONPATH
