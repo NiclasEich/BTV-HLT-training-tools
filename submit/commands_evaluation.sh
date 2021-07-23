8546ba369f769c60bf8499e19f2e4d0c7bdd4c08
Niclas Steve Eich <neich@naf-cms-gpu01.desy.de>
2021-07-23 16:38:52 +0200

    More evaluation

--- submit/commands_evaluation.sh
+++ submit/commands_evaluation.sh
@@ -11,3 +11,6 @@ pwd
 echo "python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/$TrainingVersion/KERAS_model.h5 $OnlineTrainingFiles $OnlineDirectory/training_filelist.txt $TrainingOutput/${TrainingVersion}_pred"
 #python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py -h
 python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/$TrainingVersion/KERAS_check_best_model.h5 $OnlineTrainingFiles $OnlineEvaluationFiles $TrainingOutput/${TrainingVersion}_pred -b 1000
+python3 DeepJet/scripts/plot_roc.py
+python3 plotting/plot_roc.py
+python3 plotting/plot_history.py
