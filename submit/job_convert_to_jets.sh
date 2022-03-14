a76876a462042483904310040810180df71731d8
Niclas Eich <niclas2102@gmail.com>
2022-03-14 10:50:55 +0100

    submit file

--- submit/job_convert_to_jets.sh
+++ submit/job_convert_to_jets.sh
@@ -4,7 +4,7 @@ echo "$BTVHLTToolsDirectory"
 proc=$condor_proc
 
 base=/nfs/dust/cms/user/neich/BTV/nTuple-Producer/files/run3_220103/test
-out=/nfs/dust/cms/user/neich/BTV/nTuple-Producer/files/run3_220103_jet_shaped/test/
+out=/nfs/dust/cms/user/neich/BTV/nTuple-Producer/files/run3_220103_jet_shaped_02_deepCSV/test/qcd
 
 mkdir $out
 
@@ -16,5 +16,5 @@ num=$(printf "%02d" $1)
 
 file=${base}/qcd/qcd_${num}.root
 echo "Processing $file"
-echo "python3 -u ${BTVHLTToolsDirectory}/scripts/convert_events_to_jets.py -i $file -o $out --key all"
-python3 -u $BTVHLTToolsDirectory/scripts/convert_events_to_jets.py -i $file -o $out --key qcd
+# echo "python3 -u ${BTVHLTToolsDirectory}/scripts/convert_events_to_jets.py -i $file -o $out --key all"
+python3 -u $BTVHLTToolsDirectory/scripts/convert_events_to_jets.py -i $file -o $out --key qcd --deepCSV
