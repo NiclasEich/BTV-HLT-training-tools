14b484de400a7d1e64d57ad8cf8827b4e8e8b2a3
Niclas Eich <niclas2102@gmail.com>
2022-03-14 10:50:14 +0100

    submit files

--- submit/commands_evaluation.sh
+++ submit/commands_evaluation.sh
@@ -2,16 +2,28 @@
 echo "Pythonpath:"
 echo $PYTHONPATH
 echo "Sourcing DeepJet env"
-source ${BTVHLTToolsDirectory}/setup.sh
+# source ${BTVHLTToolsDirectory}/setup.sh
 echo "Starting Training"
 echo "Pythonpath:"
 echo $PYTHONPATH
 echo "current path:"
 pwd
 echo "$BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/$TrainingVersion/KERAS_check_best_model.h5 $OnlineTrainingFiles $OnlineEvaluationFiles $TrainingOutput/${TrainingVersion}_pred -b 1000"
-python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/$TrainingVersion/KERAS_check_best_model.h5 $OnlineTrainingFiles $OnlineEvaluationFiles $TrainingOutput/${TrainingVersion}_pred -b 1000
-python3 $BTVHLTToolsDirectory/DeepJet/scripts/plot_roc.py
-python3 $BTVHLTToolsDirectory/plotting/plot_roc.py
+mkdir $TrainingOutput/${TrainingVersion}_pred
+
+python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${TrainingVersion}/KERAS_check_best_model.h5 $OnlineTrainingFiles $OnlineEvaluationFiles_QCD $TrainingOutput/${TrainingVersion}_pred/QCD -b 1000
+python3 $BTVHLTToolsDirectory/DeepJet/scripts/plot_roc.py --input_dir $TrainingOutput/${TrainingVersion}_pred/QCD
+python3 $BTVHLTToolsDirectory/plotting/plot_roc.py -i $TrainingOutput/${TrainingVersion}_pred/QCD -o $TrainingOutput/${TrainingVersion}_pred --tag QCD
+
+
+python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${TrainingVersion}/KERAS_check_best_model.h5 $OnlineTrainingFiles $OnlineEvaluationFiles_ttbar $TrainingOutput/${TrainingVersion}_pred/ttbar -b 1000
+python3 $BTVHLTToolsDirectory/DeepJet/scripts/plot_roc.py --input_dir $TrainingOutput/${TrainingVersion}_pred/ttbar
+python3 $BTVHLTToolsDirectory/plotting/plot_roc.py -i $TrainingOutput/${TrainingVersion}_pred/ttbar -o $TrainingOutput/${TrainingVersion}_pred --tag ttbar
+
+python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${TrainingVersion}/KERAS_check_best_model.h5 $OnlineTrainingFiles $OnlineEvaluationFiles $TrainingOutput/${TrainingVersion}_pred/all -b 1000
+python3 $BTVHLTToolsDirectory/DeepJet/scripts/plot_roc.py --input_dir $TrainingOutput/${TrainingVersion}_pred/all
+python3 $BTVHLTToolsDirectory/plotting/plot_roc.py -i $TrainingOutput/${TrainingVersion}_pred/all -o $TrainingOutput/${TrainingVersion}_pred --tag all
+
 python3 $BTVHLTToolsDirectory/plotting/plot_history.py
 python3 $BTVHLTToolsDirectory/plotting/plot_efficiency_along_axis.py -i $TrainingOutput/${TrainingVersion}_pred -o $TrainingOutput/${TrainingVersion}_pred/plots
 python3 $BTVHLTToolsDirectory/scripts/convert_to_onnx.py --infile $TrainingOutput/$TrainingVersion/KERAS_check_best_model.h5 --outfile $TrainingOutput/${TrainingVersion}_pred/DeepJet_model.onnx
--- submit/commands_evaluation_deepCSV.sh
+++ submit/commands_evaluation_deepCSV.sh
@@ -2,7 +2,7 @@
 echo "Pythonpath:"
 echo $PYTHONPATH
 echo "Sourcing DeepJet env"
-source ${BTVHLTToolsDirectory}/setup.sh
+# source ${BTVHLTToolsDirectory}/setup.sh
 echo "Starting Training"
 echo "Pythonpath:"
 echo $PYTHONPATH
@@ -12,7 +12,7 @@ echo "$BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${Trainin
 
 mkdir $TrainingOutput/${TrainingVersion}_deepCSV_pred
 
-python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${TrainingVersion}_deepCSV/KERAS_check_best_model.h5 $OnlineTrainingFilesDeepCSV $OnlineEvaluationFilesDeepCSV $TrainingOutput/${TrainingVersion}_deepCSV_pred/all -b 1000
+ python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${TrainingVersion}_deepCSV/KERAS_check_best_model.h5 $OnlineTrainingFilesDeepCSV $OnlineEvaluationFilesDeepCSV $TrainingOutput/${TrainingVersion}_deepCSV_pred/all -b 1000
 python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${TrainingVersion}_deepCSV/KERAS_check_best_model.h5 $OnlineTrainingFilesDeepCSV $OnlineEvaluationFilesDeepCSV_QCD $TrainingOutput/${TrainingVersion}_deepCSV_pred/QCD -b 1000
 python3 $BTVHLTToolsDirectory/DeepJetCore/bin/predict.py $TrainingOutput/${TrainingVersion}_deepCSV/KERAS_check_best_model.h5 $OnlineTrainingFilesDeepCSV $OnlineEvaluationFilesDeepCSV_ttbar $TrainingOutput/${TrainingVersion}_deepCSV_pred/ttbar -b 1000
 
@@ -23,6 +23,7 @@ python3 $BTVHLTToolsDirectory/DeepJet/scripts/plot_roc.py --deepCSV --input_dir 
 python3 $BTVHLTToolsDirectory/plotting/plot_roc.py -i $TrainingOutput/${TrainingVersion}_deepCSV_pred/all -o $TrainingOutput/${TrainingVersion}_deepCSV_pred --deepCSV --tag all
 python3 $BTVHLTToolsDirectory/plotting/plot_roc.py -i $TrainingOutput/${TrainingVersion}_deepCSV_pred/ttbar -o $TrainingOutput/${TrainingVersion}_deepCSV_pred --deepCSV --tag ttbar
 python3 $BTVHLTToolsDirectory/plotting/plot_roc.py -i $TrainingOutput/${TrainingVersion}_deepCSV_pred/QCD -o $TrainingOutput/${TrainingVersion}_deepCSV_pred --deepCSV --tag QCD
+
 python3 $BTVHLTToolsDirectory/plotting/plot_efficiency_along_axis.py -i $TrainingOutput/${TrainingVersion}_deepCSV_pred -o $TrainingOutput/${TrainingVersion}_deepCSV_pred/plots --deepCSV
 python3 $BTVHLTToolsDirectory/plotting/plot_history.py -i $TrainingOutput/${TrainingVersion}_deepCSV/ -o $TrainingOutput/${TrainingVersion}_deepCSV_pred
 ./scripts/convert_deepCSV.sh ${TrainingOutput}/${TrainingVersion}_deepCSV_pred
